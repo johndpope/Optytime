7c34a7710f4bf10ae135b7cb205b704f1175707d
Alexey Khan <khan@jufy.ru>
2014-10-03 20:44:29 +0400

    Initial Commit

---
+++ Optytime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Optytime
+//
+//  Created by Alexey Khan on 03.10.14.
+//  Copyright (c) 2014 Alexey Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
